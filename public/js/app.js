0b46d08be5a1b2ec7217999e866d8f216720d58e
misairu0511 <angas1118@gmail.com>
2026-07-07 15:30:48 +0900

    ステージ変更時に動いてしまうバグ

--- public/js/app.js
+++ public/js/app.js
@@ -44,9 +44,16 @@ if (stageSelect) {
         btnShowHint.disabled = false;
         btnShowHint.innerHTML = `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>\n          ゴースト再生`;
       }
+      
+      // アニメーションループが停止するのを少し待ってからステージを切り替える
+      setTimeout(() => {
+        engine.loadStage(e.target.value);
+        clearOutput();
+      }, 150);
+    } else {
+      engine.loadStage(e.target.value);
+      clearOutput();
     }
-    engine.loadStage(e.target.value);
-    clearOutput();
   });
 }
 
